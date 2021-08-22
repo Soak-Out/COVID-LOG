f2b203ce6a76ccad70f19a35860aaa6cff38ef8d
Sacky <yuji08752@gmail.com>
2021-08-22 21:14:41 +0900

    firebase.js作成

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrsqcPg22GcYQIGUMMKxHiAaycTT4-8GQ",
+  authDomain: "soak-out.firebaseapp.com",
+  projectId: "soak-out",
+  storageBucket: "soak-out.appspot.com",
+  messagingSenderId: "239310842026",
+  appId: "1:239310842026:web:d36d058bbf127774c708d3",
+  measurementId: "G-QQ3TSNDVPJ",
+};
+
+firebase.initializeApp(firebaseConfig);
