2f1d75e52e5f047252fad8efe7cf228a5abb6a34
kota1218 <okota1218@icloud.com>
2021-08-24 08:49:22 +0900

    Post.vueを進行しました。

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/post-page",
+    name: "Post-Page",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Post.vue"),
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Post.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <div class="flex-cl">
+      お名前<input type="text" v-model="handleName" />タイトル<input
+        type="text"
+        v-model="postTitle"
+      />
+      投稿内容<textarea type="text" v-model="postText" id="post-text" />
+      <input type="checkbox" v-model="infection" id="infection" /><label
+        for="infection"
+        >コロナに感染している/したことがある</label
+      >
+      <input type="checkbox" v-model="vaccine" id="vaccine" /><label
+        for="vaccine"
+        >ワクチン接種</label
+      >
+      <label for="illLevel">重症度</label>
+      <input
+        type="range"
+        v-model="illLevel"
+        min="1"
+        max="5"
+        step="1"
+        id="illLevel"
+      />
+      <div class="symptoms">
+        <input type="checkbox" v-model="fever" id="fever" /><label for="fever"
+          >発熱</label
+        >
+        <input type="checkbox" v-model="soreThroat" id="soreThroat" /><label
+          for="soreThroat"
+          >空咳・喉の痛み</label
+        >
+        <input
+          type="checkbox"
+          v-model="respiratoryOrgan"
+          id="respiratoryOrgan"
+        /><label for="respiratoryOrgan">呼吸器障害</label>
+        <input type="checkbox" v-model="diarrhea" id="diarrhea" /><label
+          for="diarrhea"
+          >下痢</label
+        >
+        <input type="checkbox" v-model="headache" id="headache" /><label
+          for="headache"
+          >頭痛</label
+        >
+        <input
+          type="checkbox"
+          v-model="tasteOrDisappearance"
+          id="tasteOrDisappearance"
+        /><label for="tasteOrDisappearance">味覚または嗅覚の消失</label>
+        <input type="checkbox" v-model="other" id="other" /><label for="other"
+          >その他</label
+        >
+      </div>
+
+      <button onclick="kaigyou();" v-on:click="post" @click="openModal">
+        投稿
+      </button>
+      <modal v-show="showContent" @close="closeModal" />
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+import Modal from "../components/Modal.vue"
+
+function initialState() {
+  return {
+    handleName: "",
+    postTitle: "",
+    postText: "",
+    infection: false,
+    vaccine: false,
+    illLevel: 0,
+    fever: false,
+    soreThroat: false,
+    respiratoryOrgan: false,
+    diarrhea: false,
+    headache: false,
+    tasteOrDisappearance: false,
+    other: false,
+    showContent: false,
+  }
+}
+
+export default {
+  components: {
+    Modal,
+  },
+  data() {
+    return initialState()
+  },
+  methods: {
+    post() {
+      if (this.postTitle !== "" && this.postText !== "") {
+        firebase.firestore().collection("posts").add({
+          handleName: this.handleName,
+          title: this.postTitle,
+          text: this.postText,
+          infection: this.infection,
+          vaccine: this.vaccine,
+          illLevel: this.illLevel,
+          fever: this.fever,
+          soreThroat: this.soreThroat,
+          respiratoryOrgan: this.respiratoryOrgan,
+          diarrhea: this.diarrhea,
+          headache: this.headache,
+          tasteOrDisappearance: this.tasteOrDisappearance,
+          other: this.other,
+        })
+        Object.assign(this.$data, initialState())
+      }
+    },
+    openModal() {
+      this.showContent = true
+    },
+    closeModal() {
+      this.showContent = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.flex-cl {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 500px;
+  margin-bottom: 100px;
+}
+input {
+  height: 1rem;
+  margin-bottom: 20px;
+}
+.symptoms {
+  padding: 30px;
+}
+</style>
